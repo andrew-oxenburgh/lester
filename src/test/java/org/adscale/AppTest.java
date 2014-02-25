98f6b97a4eff7671a0b9fa64347aa44bdc2b95ac
Andrew Oxenburgh <andrew.oxenburgh@adscale.co.nz>
2014-02-26 12:36:52 +1300

    selenium. .gitignore

--- src/test/java/org/adscale/AppTest.java
+++ src/test/java/org/adscale/AppTest.java
@@ -1,6 +1,9 @@
 package org.adscale;
 
 import static org.junit.Assert.*;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
 import org.junit.Test;
 
 public class AppTest {
@@ -8,4 +11,12 @@ public class AppTest {
 	public void testApp() {
 		assertTrue(true);
 	}
+
+
+    @Test
+    public void testName() throws Exception {
+        WebClient webClient = new WebClient();
+        Page page = webClient.getPage("http://localhost:8080/lester/helloGroovlet.groovy");
+        assertEquals(true, page.isHtmlPage());
+    }
 }
